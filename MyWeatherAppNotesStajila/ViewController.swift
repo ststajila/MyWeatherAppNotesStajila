680e12fc790a88dde71b9d6b86a02423a589023a
Stanislav <stazilastas@gmail.com>
2024-01-10 09:48:12 -0600

    Getting the temperature to show up on the screen

--- MyWeatherAppNotesStajila/ViewController.swift
+++ MyWeatherAppNotesStajila/ViewController.swift
@@ -22,52 +22,43 @@ class ViewController: UIViewController {
        //creating a session
         let session = URLSession.shared
 
-    // creating a URL for api call (use your API key)
-        let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=imperial&appid={1942debaa31f5e108a4255b2488e7584}"
+    // creating a URL for api call (use your API key, add location of the place using lat & long of a place, change units if applicable)
+        let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=42.243420&lon=-88.316978&units=imperial&appid=1942debaa31f5e108a4255b2488e7584")
     
         // Making an API call and creating data in the completion handler
-        let dataTask = session.dataTask(with: weatherURL){
+        let dataTask = session.dataTask(with: weatherURL!){
             //completion handler: happens on a different thread, could take time to take data
             (data: Data?, response: URLResponse?, error: Error?) in
-
-                        if let error = error {
-                            print("Error:\n\(error)")
-                        } else {
-                            // if there is data
-                            if let data = data {
-                                // convert data to json Object
-                                if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary {
-                                    
-                                    // print the jsonObj to see structure
-                                    print(jsonObj)
-                                    
-                                    // find main key and get all the values as a dictionary
-                                    if let mainDictionary = jsonObj.value(forKey: "main") as? NSDictionary {
-                                        
-                                        // get the value for the key temp
-                                        if let temperature = mainDictionary.value(forKey: "temp") {
-                                            // make it happen on the main thread so it happens during viewDidLoad
-                                            DispatchQueue.main.async {
-                                                // making the value show up on a label
-                                                self.weatherLabel.text = "result \(temperature)"
-                                            }
-                                          
-                                        } else {
-                                            print("Error: unable to find temperature in dictionary")
-                                        }
-                                    } else {
-                                        print("Error: unable to convert json data")
-                                    }
+            
+            if let e = error {
+                print("Error:\n\(e)")
+            } else {
+                // if there is data
+                if let d = data {
+                    // convert data to json Object
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .allowFragments) as? NSDictionary {
+                        
+                        // print the jsonObj to see structure
+                        print(jsonObj)
+                        
+                        // needs as since return type Any
+                        if let main = jsonObj.value(forKey: "main") as? NSDictionary {
+                            if let temp = main.value(forKey: "temp") as? Double{
+                                //Making it happen on the main thread, gets data before shows up on the label
+                                DispatchQueue.main.async {
+                                    self.weatherLabel.text = "\(temp)"
                                 }
-                                else {
-                                    print("Error: Can't convert data to json object")
-                                }
-                            }else {
-                                print("Error: did not receive data")
                             }
                         }
+                        
+                        
+                        
                     }
-
+                }
+            }
+        }
+                                    
+            // Tryes to get the data each time!
                     dataTask.resume()
         }
 }
